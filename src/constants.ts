9b9a70a4e8571d610411e7687fd1e361a6b7538f
rahmanGit2417 <rahman.m@turing.com>
2025-03-21 02:31:23 +0530

    original_codebase

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+import { AllItemsPerPage } from './types';
+// new file
+export const defaultPageSizes = [10, 25, 50, 100, AllItemsPerPage];
---
+++ src/hooks/usePagination.tsx
@@ -0,0 +1,84 @@
+import { useState, useEffect, useCallback } from 'react';
+
+type PaginationResult<T> = {
+	data: T[];
+	total: number | undefined;
+	page: number;
+	hasMore: boolean;
+	nextPage: () => void;
+	prevPage: () => void;
+	setPage: (page: number) => void;
+	fetchPage: (page: number, limit: number) => Promise<void>;
+};
+
+type FetchFunction<T> = (limit: number, skip: number) => Promise<T[]>;
+
+export function usePagination<T>(
+	initialPage: number,
+	limit: number,
+	dataOrFetchFunction: T[] | FetchFunction<T>,
+): PaginationResult<T> {
+	const [data, setData] = useState<T[]>([]);
+	const [page, setPage] = useState<number>(initialPage);
+	const [hasMore, setHasMore] = useState<boolean>(true);
+	const total: number | undefined = Array.isArray(dataOrFetchFunction)
+		? dataOrFetchFunction.length
+		: undefined;
+	let serverCount: number | undefined;
+
+	const fetchPage = useCallback(
+		async (page: number): Promise<void> => {
+			const skip = (page - 1) * limit;
+			let result;
+			if (Array.isArray(dataOrFetchFunction)) {
+				result = dataOrFetchFunction.slice(skip, skip + limit);
+				serverCount = dataOrFetchFunction.length;
+				setData(result);
+			} else {
+				result = await dataOrFetchFunction(limit, skip);
+				serverCount = (result as any).serverCount;
+				setData(result);
+			}
+			if (serverCount !== undefined) {
+				setHasMore(page * limit < serverCount);
+			} else {
+				setHasMore(result.length >= limit);
+			}
+		},
+		[limit, dataOrFetchFunction],
+	);
+
+	const nextPage = () => {
+		if (hasMore) {
+			const newPage = page + 1;
+			fetchPage(newPage);
+			setPage(newPage);
+		}
+	};
+
+	const prevPage = () => {
+		if (page > 1) {
+			const newPage = page - 1;
+			fetchPage(newPage);
+			setPage(newPage);
+		}
+	};
+
+	useEffect(() => {
+		fetchPage(page);
+	}, [fetchPage, page]);
+
+	return {
+		data,
+		page,
+		hasMore,
+		nextPage,
+		prevPage,
+		total,
+		setPage: (page: number) => {
+			setPage(page);
+			fetchPage(page);
+		},
+		fetchPage,
+	};
+}
